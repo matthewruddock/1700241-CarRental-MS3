2a5ca06035b397c1a57871e8f7a901b5f1cb600a
matthewruddock <55115233+matthewruddock@users.noreply.github.com>
2020-10-29 18:48:11 -0500

    updated

--- App_Code/Order.cs
+++ App_Code/Order.cs
@@ -8,8 +8,8 @@ namespace _1700241_CarRental_MS2.App_Code
     public class Order
     {
         //OrderID, CarID, Username, StartDate, EndDate, NumberOfDays, Subtotal
-        public string OrderID { get; set; }
-        public int CarID { get; set; }
+        public int OrderID { get; set; }
+        public int id { get; set; }
         public string Username { get; set; }
         public string StartDate { get; set; }
         public string EndDate { get; set; }
@@ -18,18 +18,18 @@ namespace _1700241_CarRental_MS2.App_Code
 
         public Order()
         {
-            this.OrderID = "none";
-            this.CarID = 0;
+            this.OrderID = 0;
+            this.id = 0;
             this.Username = "none";
             this.StartDate = "none";
             this.EndDate = "none";
             this.NumberOfDays = 0;
             this.Subtotal = 0;
         }
-        public Order(string orderid, int carid, string username, string startdate, string enddate, int nod, float subtotal)
+        public Order(int orderid, int Id, string username, string startdate, string enddate, int nod, float subtotal)
         {
             this.OrderID = orderid;
-            this.CarID = carid;
+            this.id = Id;
             this.Username = username;
             this.StartDate = startdate;
             this.EndDate = enddate;
---
+++ App_Data/dbo.Table_1.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Username] NVARCHAR(MAX) NOT NULL PRIMARY KEY, 
+    [Password] NVARCHAR(MAX) NULL
+)
